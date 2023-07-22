5c8046a948a6f1739247a876ce2fe137b603e797
rlaminseok0824 <114082574+rlaminseok0824@users.noreply.github.com>
2023-07-22 23:55:48 +0900

    키워드 디자인 변경

--- src/components/Keyword.svelte
+++ src/components/Keyword.svelte
@@ -3,7 +3,6 @@
     import { fade } from "svelte/transition";
     
     export let name: string;
-    export let isDelete: boolean;
     const dispatch = createEventDispatcher();
 
     const handleDelete = (event : any) => {
@@ -14,34 +13,26 @@
 
 <span class="container" transition:fade>
     {name}
-    {#if isDelete}
-        <span on:click={handleDelete}>
+    <span on:click={handleDelete}>
             X 
-        </span>
-    {/if}
+    </span>
 </span>
 
 <style>
-    .container > span:hover{
-        cursor: pointer;
-    }
-
     .container > span {
-        margin-left: 0.5rem;
+        color: #cbd5e0;
     }
 
     .container {
-        min-width: 15%;
-        max-width: fit-content;
-        margin: 0.5rem;
-        padding: 0.15rem 0.55rem;
+        width: 100%;
+        background-color: #f7fafc;
+        padding: 0.5rem 1rem;
+        border-radius: var(--radius);
+        justify-content: space-between;
         display: flex;
-        align-items: center;
-        justify-content: center;
-        font-size: 1rem;
-        border-radius: 20px;
-        border: 1px solid var(--c-foreground-gray);
-        background-color: var(--c-foreground-gray);
-        color: gray;
+        flex-direction: row;
+        color: #1a202c;
+        margin-top: 1rem;
     }
+
 </style>
