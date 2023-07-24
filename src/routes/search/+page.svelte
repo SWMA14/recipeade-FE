8e0a4a3b2cc740b7566fbb567031772aaba8f22a
rlaminseok0824 <xornjsrlaals@gmail.com>
2023-07-24 11:31:26 +0900

    검색 초기 화면 개발

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,149 +1,31 @@
 <script lang="ts">
-    import Keyword from "$components/Keyword.svelte";
-    import { goto,afterNavigate } from "$app/navigation";
-    import { page } from "$app/stores";
-    import { base } from "$app/paths";
-    import { flyingFade,duration } from "$lib/transition";
-    import { Preferences } from "@capacitor/preferences";   
-    import { onMount } from "svelte";
-
-
-    const arrayToString = (arr: string[]) : string =>{
-        return arr.join("/");
-    }
-
-    const stringToArray = (str:string): string[] => {
-        return str.split("/");
-    }
-
-    let items : string[] = [
-        "item1","item2","item3"
-    ];
-    let searchValue = "";
-    let previousPage : string = base;
-
-    const deleteAllKeywords = () => {
-        items = [];
-        Preferences.set({ 
-            key: "keywords", 
-            value: arrayToString(items)
-        });
-    };
-
-    const deleteKeyword = (keywordToRemove: string) => {
-        items = items.filter((keyword) => keyword !== keywordToRemove);
-        Preferences.set({ 
-            key: "keywords", 
-            value: arrayToString(items)
-        });
-    };
-
-    const addKeywords = async () => {
-        if(searchValue.length > 0){
-            items.push(searchValue);
-        }
-
-        await Preferences.set({ 
-            key: "keywords", 
-            value: arrayToString(items)
-        });
-
-        goto("/");
-    }
-
-    const getKeywords =async () => {
-        const {value} = await Preferences.get({key: 'keywords'});
-        if(value){
-            items = stringToArray(value);
-        }
-    }
-
-    afterNavigate(({from}) => {
-        previousPage = from?.url.pathname || previousPage;
-    });
-
-    onMount(()=>{
-        //getKeywords();
-    });
+    import SearchModal from "$components/SearchModal.svelte";
 </script>
 
-<!-- <div class="header">
-    <a href={previousPage}>
-        <img alt="이전 화면" src="/images/icons/autonext.png"/>
-    </a>
-        <input class="search" bind:value={searchValue} placeholder="어떤 요리를 찾아 볼까요" />        
-        <button on:click={addKeywords}>
-            <img alt="이전 화면" src="/images/icons/autonext.png"/>
-        </button>
-</div> -->
 
-<div class="search-container">
-    <img alt="이전 화면" src="/images/icons/autonext.png" />
-    <input type="search" bind:value={searchValue} placeholder="어떤 요리를 해 볼까요?">
-</div>
-
+<a href="/search/clicked">
+    <div class="search-container">
+        <img alt="이전 화면" src="/images/icons/autonext.png" />
+        <input type="search" placeholder="어떤 요리를 해 볼까요?">
+    </div>
+</a>
+    
 <div class="section">
-    <div class="section-top">
-        <h2>최근 검색</h2>
-        <span on:click={deleteAllKeywords}>모두 지우기</span>
-    </div>
-    <div>
-        {#each items as item (item)}
-            <Keyword name={item} on:remove={()=>{deleteKeyword(item)}}/>
-        {/each}
+    <h2>지금 인기 있어요</h2>
+    <div class="category">
+        <SearchModal name="감자"/>
+        <SearchModal name="오뎅"/>
     </div>
 </div>
-
-<!-- 
-<div class="body">
-    {#if items.length > 0}
-        <div class="section" out:flyingFade={{duration: 500 }}>
-            <div class="topic">
-                <h2>최근 검색어</h2>
-                <div class="delete" on:click={deleteAllKeywords}>
-                    <img alt="이전 화면" src="/images/icons/autonext.png" />
-                    <span>모두 지우기</span>
-                </div>
-            </div>
-            <div class="keywords">
-                {#each items as item (item)}
-                    <Keyword name={item} on:remove={()=>{deleteKeyword(item)}}/>
-                {/each}
-            </div>
-        </div>
-    {/if}
-    <div class="section top">
-        <div class="topic">
-            <h2>실시간 인기 검색어</h2>
-            <div class="update-time">
-                <span>22:54 업데이트</span>
-            </div>
-        </div>
-        <div class="keywords">
-            {#each items as item (item.id)}
-                <Keyword name={item.name} isDelete={false} on:remove={removeItem} />
-            {/each}
-        </div>
-    </div>
-</div> -->
-
 
 <style>
-    .section-top > h2{
-        color: #1a202c;
+    a{
+        width: 100%;
     }
-
-    .section-top > span{
-        font-size: var(--body-2);
-        color: #cbd5e0;
-    }
-
-    .section-top {
+    .category {
         display: flex;
         flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        width: 100%;
+        flex-wrap: wrap;
     }
 
     .section {
@@ -153,8 +35,8 @@
     }
 
     .search-container > img {
-        width: 2rem;
-        height: 2rem;    
+        height: 1.25rem;
+        width: 1.25rem;   
         margin-right: 0.5rem;
     }
 
@@ -177,96 +59,5 @@
         margin-top: 1rem;
         margin-bottom: 2rem;
         position: relative;
-    }
-
-    .search-container  input:focus {
-        outline: none;
-    }
-
-    .search-container:focus-within::before{
-        display: block;
-        content: "";
-        position:absolute;
-        top: -1px;
-        bottom: -1px;
-        left: -1px;
-        right: -1px;
-        border: 2px solid orange;
-        border-radius: var(--radius);
-    }
-
-
-
-
-    .body {
-        position: relative;
-        width: 100%;
-    }
-
-    .keywords {
-        flex: 0 0 auto;
-        padding-left: 0.25rem;
-        display: flex;
-        align-items: center;
-        justify-content: left;
-        flex-wrap: wrap;
-    }
-
-
-    .delete:hover {
-        cursor: pointer;
-    }
-
-    .delete > img {
-        width: 1rem;
-        height: 1rem;
-        margin-right: 0.25rem;
-    }
-
-    .update-time,
-    .delete {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        color: var(--c-foreground-gray);
-    }
-
-    .topic {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        width: 100%;
-        margin-top: 5rem;
-    }
-
-    .header {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-evenly;
-        align-items: center;
-        width: 100%;
-    }
-
-
-    .header > button > img,
-    .header > a {
-        transform: scaleX(-1);
-        width: 7.5%;
-        cursor: pointer;
-    }
-
-    .search {
-        width: 65%;
-        border-radius: var(--radius);
-        padding-top: 0.5rem;
-        padding-bottom: 0.5rem;
-        padding-left: 1rem;
-        padding-right: 1rem;
-        background-color: var(--c-foreground-gray);
-    }
-
-    ::placeholder {
-        color: gray;
-        font-size: 1rem;
     }
 </style>
