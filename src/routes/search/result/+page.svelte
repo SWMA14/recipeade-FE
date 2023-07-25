9a1456ae4cff78fa71ae11dadb4fceb1287f3e6f
rlaminseok0824 <xornjsrlaals@gmail.com>
2023-07-25 16:04:56 +0900

    필터링모델 결과페이지 적용

--- src/routes/search/result/+page.svelte
+++ src/routes/search/result/+page.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
     import { page } from "$app/stores";
     import { onMount } from "svelte";
+    import FilterModal from "$components/FilterModal.svelte";
+    import { goto } from "$app/navigation";
 
     let value: any;
+
+    const updateSearchParams = (key: string, value: string) =>{
+        const searchParams = new URLSearchParams($page.url.search);
+        searchParams.set(key, value);
+        goto('/search/result?'+searchParams.toString());
+    }
+
 
     onMount(()=>{
         value = $page.url.searchParams.get("query")
@@ -23,11 +31,22 @@
     </a>
 </div>
 
+<div class="section">
+    <FilterModal name="최신" on:click={()=>{updateSearchParams("sortby","최신")}}/>
+    <FilterModal name="평점" on:click={()=>{updateSearchParams("sortby","평점")}}/>
+    <FilterModal name="조회" on:click={()=>{updateSearchParams("sortby","조회")}}/>
+</div>
+
 
 <style>
     .input {
         width: 100%;
-          
+    }
+
+    .section {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
     }
     
     .search-container > a > img {
@@ -35,14 +54,6 @@
         width: 1.25rem;
         margin-right: 0.5rem;
         transform: scaleX(-1);
-    }
-
-    .search-container > a > input {
-        font-size: 1.25rem;
-        width: 100%;
-        border: none;
-        font-weight: 500;
-        background-color: transparent;
     }
 
     .search-container {
