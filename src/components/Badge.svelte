1e9c403754f74d55bb72b17a73f5e0707b7c6a9d
DaveRipper <daveripper0020@gmail.com>
2023-07-26 14:07:40 +0900

    Badge 컴포넌트 추가

---
+++ src/components/Badge.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let dark = false;
+    export let rightMargin = false;
+</script>
+
+<div class="typo-body-2" class:dark={dark} class:rightMargin={rightMargin}>
+    <slot />
+</div>
+
+<style>
+    div {
+        padding: var(--space-xxxs) var(--space-xs);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: var(--gray-800);
+        background-color: var(--white);
+        border-radius: var(--radius-big);
+    }
+
+    .dark {
+        background-color: var(--gray-100);
+    }
+
+    .rightMargin {
+        margin-right: var(--space-xxs);
+    }
+</style>
