2dff12039ae58598217e9c6f8302d5437ab2e4c0
DaveRipper <daveripper0020@gmail.com>
2023-07-26 13:57:23 +0900

    Card 컴포넌트 추가

---
+++ src/components/Card.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    export let video = undefined;
+    export let noRadius = false;
+    export let noPadding = false;
+    export let columnFlex = false;
+    export let rightMargin = false;
+    export let bottomMargin = false;
+    export let scrollSnap = false;
+</script>
+
+<div class:no-radius={noRadius} class:no-padding={noPadding} class:column-flex={columnFlex}
+    class:right-margin={rightMargin} class:bottom-margin={bottomMargin} class:scroll-snap={scrollSnap}>
+    <slot />
+</div>
+
+<style>
+    div {
+        width: 100%;
+        min-width: calc(100% - var(--space-xl));
+        padding: var(--space-xs);
+        display: flex;
+        flex-direction: column;
+        position: relative;
+        background-color: var(--gray-100);
+        border-radius: var(--radius);
+    }
+
+    .no-radius {
+        border-radius: 0;
+    }
+
+    .no-padding {
+        padding: 0;
+    }
+
+    .column-flex {
+        flex-direction: column;
+    }
+
+    .right-margin {
+        margin-right: var(--space-xs);
+    }
+
+    .bottom-margin {
+        margin-bottom: var(--space-xs);
+    }
+
+    .scroll-snap {
+        scroll-snap-align: start;
+        scroll-snap-stop: always;
+    }
+</style>
