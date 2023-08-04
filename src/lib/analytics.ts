cbb1da180e30673e3583aeb5822bc73494fef8b8
rlaminseok0824 <xornjsrlaals@gmail.com>
2023-08-04 19:07:59 +0900

    firebase 빌드 오류 해결

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -1,31 +1,10 @@
 // Init for the web
 import {FirebaseAnalytics} from '@capacitor-community/firebase-analytics';
-import { Device } from '@capacitor/device';
 
 export class AnalyticsService {
   analyticsEnabled = true;
 
-  constructor() {
-    this.toggleAnalytics();
-  }
-
-  async initFb() {
-    Device.getInfo()
-        .then(info => {
-            if (info.platform === "web"){
-                FirebaseAnalytics.initializeFirebase({
-                    apiKey: "AIzaSyDAPxRiDSx7VPJJN-85cco7oZTm2XFhOqY",
-                    authDomain: "recipeade-85add.firebaseapp.com",
-                    projectId: "recipeade-85add",
-                    storageBucket: "recipeade-85add.appspot.com",
-                    messagingSenderId: "273092375018",
-                    appId: "1:273092375018:web:fccff27c95104fc38a8293",
-                    measurementId: "G-S8ZQJSGB9G"
-                  });
-            }
-        })
-        .catch(err => {console.log(err)});
-  }
+  constructor() {}
 
   async logEvent(name:string,param: object) {
     await FirebaseAnalytics.logEvent({
@@ -41,7 +20,6 @@ export class AnalyticsService {
   }
 
   toggleAnalytics() {
-    this.analyticsEnabled = !this.analyticsEnabled;
     FirebaseAnalytics.setCollectionEnabled({
       enabled: true,
     });
