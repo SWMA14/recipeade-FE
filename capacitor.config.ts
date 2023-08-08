334e0a465967bb5822fc63f6035da71f7ecbb138
daveripper0020 <daveripper0020@gmail.com>
2023-08-08 15:09:46 +0900

    앱 이름 한글로 변경

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.recipeade.svelte',
-  appName: 'recipeade',
+  appName: '레시피에이드',
   webDir: '.svelte-kit/cloudflare',
   server: {
     // url: "http://172.16.101.141:5173",
