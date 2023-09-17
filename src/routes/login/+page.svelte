39b8f6d2fa4d6f2c1e68192a8b8ab1250c53aaa6
rlaminseok0824 <xornjsrlaals@gmail.com>
2023-09-17 17:48:01 +0900

    oauth 백엔드 주석 처리

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,7 +2,10 @@
     import { onMount } from "svelte";
     import Device from "svelte-device-info";
     import { MetaTags } from "svelte-meta-tags";
-
+    import Button from "$components/Button.svelte";
+    import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+    import { goto } from "$app/navigation";
+    
     const title = "로그인 페이지";
     const description = "로그인 페이지입니다.";
 
@@ -19,66 +22,84 @@
     canonical=""
 />
 
+<div class="margin"></div>
 <h1>
     레시피에이드
 </h1>
 
-
-<div class="loginsec">
-    <form class="login-form" method="post" action="?/OAuth2">
-        <button class="login-btn google" type="submit">
+    <Button icon={faEnvelope} on:click={()=>{goto('/login/email')}}>
+        이메일로 계속하기
+    </Button>
+    <div class="separator">
+        또는
+     </div>
+     <form class="login-form" method="post" action="?/OAuth2">
+        <Button kind="white">
             <img class="google-logo" alt="구글 로고" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/32px-Google_%22G%22_Logo.svg.png" />
             구글로 계속하기
-        </button>
-    </form>
-    <button class="login-btn kakao">카카오로 계속하기</button>
-    <a href="/" class="login-form">
-        <button class="login-btn">로그인 없이 계속하기</button>
-    </a>
-</div>
+        </Button>
+     </form>
+    <div class="margin"></div>
+    <Button kind="success">
+        네이버로 계속하기    
+    </Button>
+    <div class="margin"></div>
 
+
+<span class="agreement">
+    서비스를 이용함으로써 귀하는 본 
+    <span class="underline">이용약관</span>과 
+</span>
+<span class="agreement">
+    <span class="underline">개인정보처리방침</span>에 동의하게 됩니다.
+</span>
 
 
 <style>
+    .underline {
+        text-decoration: underline;
+    }
+
+    .agreement {
+        font-size: 0.9rem;
+        text-align: center;
+        position: relative;
+        justify-content: center;
+    }
+    
+
+    .margin {
+        margin-bottom: 1rem;
+    }
+
+    .separator {
+        width: 100%;
+        text-align: center;
+        margin: 20px 0;
+        position: relative;
+    }
+
+    .separator::before, .separator::after {
+        content: '';
+        position: absolute;
+        width: 40%;
+        border-bottom: 1px solid #000; /* 구분선 색상 */
+    }
+
+    .separator::before {
+        left: 0;
+        bottom: 50%;
+    }
+
+    .separator::after {
+        right: 0;
+        bottom: 50%;
+    }
+
     .google-logo{
         position: absolute;
         padding-left: 1rem;
-        left: 0%;
-    }
-    .google {
-        background-color: white;
+        left: 5%;
     }
 
-    .kakao {
-        background-color: #FEE500;
-    }
-
-    .login-form{
-        width: 100%;
-    }
-
-    .loginsec {
-        position: absolute;
-        bottom: 0rem;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-end;
-        width: 75%;
-    }
-
-
-    .login-btn {
-        margin-bottom: 1rem;
-        padding: 0.75rem 1.5rem;
-        color: black;
-        border: none;
-        border-radius: 5px;
-        font-size: 16px;
-        cursor: pointer;
-        width: 100%;
-        left: 50%;
-        text-align: center;
-        font-size: large;
-    }
 </style>
---
+++ src/routes/login/email/+page.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    import Input from "$components/Input.svelte";
+
+
+    let email : string;
+    let password : string;
+    let isEmail : boolean = false;
+
+    const ChangeEmail = (value: string) => {
+        email = value;
+    }
+
+    const ChangePassword = (value: string) => {
+        password = value;
+    }   
+
+    const validateEmail = () => {
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if(regex.test(email)){
+            //backend 서버에 있는 회원인지 아닌지 확인 후
+            //있으면 password 일치 확인 후 로그인
+            //틀리면 password 생성
+            isEmail = true;
+        }
+        else{
+            isEmail = false;
+        }
+    }
+</script>
+
+
+<h1>
+    이메일로 계속하기
+</h1>
+<div class="margin"></div>
+<div style="width: 90%;">
+    <Input placeholder="이메일을 작성해주세요" valueChanged={ChangeEmail} value={email} iconClicked={validateEmail}/>
+    <div class="margin"></div>
+    {#if isEmail}
+        <Input placeholder="비밀번호를 작성해주세요" valueChanged={ChangePassword} value={password}/>
+    {/if}
+</div>
+
+
+
+<style>
+    .margin {
+        margin-bottom: 3rem;
+    }
+</style>
+
+
+
