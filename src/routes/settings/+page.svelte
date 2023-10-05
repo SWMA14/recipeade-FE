42057b2e8145e7e04b6b7765861ad2664163ed14
daveripper0020 <daveripper0020@gmail.com>
2023-10-06 04:00:14 +0900

    설정 페이지 추가

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { _ } from "svelte-i18n";
+    import { getContext } from "svelte";
+    import type { Writable } from "svelte/store";
+    import type { DynamicBarContext } from "$lib/dynamicBar";
+    import Button from "$components/Button.svelte";
+    import upperMain from "./__upperBarComponents/main.svelte";
+    import lowerMain from "../__lowerBarComponents/main.svelte";
+
+    getContext<Writable<DynamicBarContext>>("upperBar").update(x => x = {
+        isBackgroundShown: true,
+        main: upperMain
+    });
+    getContext<Writable<DynamicBarContext>>("lowerBar").update(x => x = {
+        main: lowerMain
+    });
+</script>
+
+<div>
+    <Button kind="gray" bottomMargin="2xs">{$_("page.settings.signOut")}</Button>
+    <Button kind="danger">{$_("page.settings.deleteAccount")}</Button>
+</div>
+
+<style>
+    div {
+        width: 100%;
+        margin-top: calc(var(--space-3xl) + env(safe-area-inset-top));
+    }
+</style>
---
+++ src/routes/settings/__upperBarComponents/main.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { _ } from "svelte-i18n";
+</script>
+
+<div>
+    <h2>{$_("page.settings.heading")}</h2>
+</div>
+
+<style>
+    div {
+        height: 100%;
+        display: flex;
+        align-items: center;
+    }
+</style>
