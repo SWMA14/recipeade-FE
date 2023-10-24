90d3318a7f58ad5f49d69d6ec6564ec65689e17a
daveripper0020 <daveripper0020@gmail.com>
2023-10-25 00:59:03 +0900

    Modal 컴포넌트 추가

---
+++ src/components/Modal.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import { fade, fly } from "svelte/transition";
+    import { cubicIn, cubicOut } from "svelte/easing";
+
+    export let shown: boolean;
+
+    function hide()
+    {
+        shown = false;
+    }
+</script>
+
+{#if shown}
+    <div class="modal" in:fly={{ y: 50, easing: cubicOut, duration: 500 }} out:fade={{ duration: 250 }}>
+        <slot />
+    </div>
+    <div class="overlay" on:keydown={hide} on:click={hide} transition:fade={{ duration: 250 }} />
+{/if}
+
+<style lang="postcss">
+    .modal {
+        width: 85%;
+        max-width: var(--max-width);
+        height: fit-content;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 1002;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .overlay {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 1001;
+        background-color: rgba(0, 0, 0, 0.65);
+        overflow-x: hidden;
+    }
+</style>
