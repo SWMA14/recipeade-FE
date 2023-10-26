d4f0e8ef8d48bf966b8cbafb67d0196e07205508
daveripper0020 <daveripper0020@gmail.com>
2023-10-27 04:46:13 +0900

    AsymmetricGrid 구현 방식 변경

--- src/components/AsymmetricGrid.svelte
+++ src/components/AsymmetricGrid.svelte
@@ -1,26 +1,51 @@
 <script lang="ts">
-    function firstChild(node: HTMLElement)
-    {
-        const offset = (node.firstChild as HTMLElement).clientHeight / 2;
+    let firsts: HTMLElement[] = [];
+    let seconds: HTMLElement[] = [];
+    let offset: number;
 
-        for (let i = 1; i < node.children.length; i += 2)
+    function splitChildren(node: HTMLElement)
+    {
+        offset = (node.firstChild as HTMLElement).clientHeight / 2;
+
+        for (let i = 0; i < node.children.length; i++)
         {
             const element = node.children[i] as HTMLElement;
-            element.style.marginTop = `${offset}px`;
-            element.style.marginBottom = `-${offset}px`;
+
+            if (i < node.children.length - 2)
+                element.style.marginBottom = "var(--space-xs)";
+
+            if (i % 2 === 0)
+                firsts.push(element);
+            else
+                seconds.push(element);
         }
+    }
+
+    function setFirstChildren(node: HTMLElement)
+    {
+        node.replaceChildren(...firsts);
+    }
+
+    function setSecondChildren(node: HTMLElement)
+    {
+        node.style.marginTop = `${offset}px`;
+        node.replaceChildren(...seconds);
     }
 </script>
 
-<div use:firstChild>
+<div use:splitChildren>
     <slot />
 </div>
+<div class="wrapper">
+    <div class="first" use:setFirstChildren />
+    <div class="second" use:setSecondChildren />
+</div>
 
-<style>
-    div {
+<style lang="postcss">
+    .wrapper {
         width: 100%;
         display: grid;
-        grid-template-columns: 1fr 1fr;
+        grid-template-columns: calc(50% - var(--space-2xs)) calc(50% - var(--space-2xs));
         gap: var(--space-xs);
     }
 </style>
