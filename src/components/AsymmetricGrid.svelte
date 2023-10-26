9950b365ab0da8a2258047c05ab86c6e85e20edc
daveripper0020 <daveripper0020@gmail.com>
2023-10-27 03:45:44 +0900

    AsymmetricGrid 컴포넌트 추가

---
+++ src/components/AsymmetricGrid.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    function firstChild(node: HTMLElement)
+    {
+        const offset = (node.firstChild as HTMLElement).clientHeight / 2;
+
+        for (let i = 1; i < node.children.length; i += 2)
+        {
+            const element = node.children[i] as HTMLElement;
+            element.style.marginTop = `${offset}px`;
+            element.style.marginBottom = `-${offset}px`;
+        }
+    }
+</script>
+
+<div use:firstChild>
+    <slot />
+</div>
+
+<style>
+    div {
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: var(--space-xs);
+    }
+</style>
