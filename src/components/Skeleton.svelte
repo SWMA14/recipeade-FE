3641dec837ffb42295a60ac62a217e0f414d86e0
daveripper0020 <daveripper0020@gmail.com>
2023-11-05 04:29:38 +0900

    Skeleton 구조 변경

--- src/components/Skeleton.svelte
+++ src/components/Skeleton.svelte
@@ -5,24 +5,27 @@
     export let lines = 1;
     export let leftMargin: SpaceType | undefined = undefined;
     export let rightMargin: SpaceType | undefined = undefined;
+    export let topMargin: SpaceType | undefined = undefined;
     export let bottomMargin: SpaceType | undefined = undefined;
 
     let leftMarginValue = leftMargin ? `var(--space-${leftMargin})` : undefined;
     let rightMarginValue = rightMargin ? `var(--space-${rightMargin})` : undefined;
+    let topMarginValue = topMargin ? `var(--space-${topMargin})` : undefined;
     let bottomMarginValue = bottomMargin ? `var(--space-${bottomMargin})` : undefined;
 </script>
 
-{#if lines > 1}
-    {#each Array(lines - 1) as _}
-        <div class="{kind} bottom-margin" style="--bottom-margin: var(--space-xs);" />
-    {/each}
-{/if}
-<div class={kind} class:partial={kind !== "slot"}
-    class:left-margin={leftMargin} class:right-margin={rightMargin} class:bottom-margin={bottomMargin}
-    style="--left-margin: {leftMarginValue}; --right-margin: {rightMarginValue}; --bottom-margin: {bottomMarginValue};">
-    {#if kind === "slot"}
-        <slot />
+<div class="container" class:left-margin={leftMargin} class:right-margin={rightMargin} class:top-margin={topMargin} class:bottom-margin={bottomMargin}
+    style="--left-margin: {leftMarginValue}; --right-margin: {rightMarginValue}; --top-margin: {topMarginValue}; --bottom-margin: {bottomMarginValue};">
+    {#if lines > 1}
+        {#each Array(lines - 1) as _}
+            <div class="skeleton {kind} bottom-margin" style="--bottom-margin: var(--space-xs);" />
+        {/each}
     {/if}
+    <div class="skeleton {kind}" class:partial={kind !== "slot"}>
+        {#if kind === "slot"}
+            <slot />
+        {/if}
+    </div>
 </div>
 
 <style lang="postcss">
@@ -36,7 +39,15 @@
         }
     }
 
-    div {
+    .container {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+
+    .skeleton {
+        width: 100%;
         background-color: var(--gray-100);
         animation: flicker 2s infinite alternate;
         border-radius: var(--radius);
@@ -72,6 +83,10 @@
 
     .right-margin {
         margin-right: var(--right-margin);
+    }
+
+    .top-margin {
+        margin-top: var(--top-margin);
     }
 
     .bottom-margin {
