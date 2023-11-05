0c5ad3e9ad9667c15999990d5f4abdb7827755ac
daveripper0020 <daveripper0020@gmail.com>
2023-11-05 21:14:04 +0900

    AlertDrawer 컴포넌트 추가

---
+++ src/components/AlertDrawer.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import { _ } from "svelte-i18n";
+    import { getContext } from "svelte";
+    import type { Writable } from "svelte/store";
+    import type { DynamicBarContext } from "$lib/dynamicBar";
+    import Drawer from "$components/Drawer.svelte";
+    import main from "./__confirmationDrawerLowerBarComponents/main.svelte";
+
+    export let heading: string | undefined = undefined;
+    export let text: string | undefined = undefined;
+    export let shown = false;
+    export let show: (() => void) | undefined = undefined;
+    export let hide: (() => void) | undefined = undefined;
+    export let confirmText = $_("page.confirm");
+
+    const lowerBar = getContext<Writable<DynamicBarContext>>("lowerBar");
+
+    let cached: DynamicBarContext = {};
+
+    function updateLowerBar()
+    {
+        cached = $lowerBar;
+        lowerBar.update(x => x = {
+            main,
+            mainProps: {
+                text: confirmText,
+                hide
+            }
+        });
+    }
+
+    function revert()
+    {
+        lowerBar.update(x => x = cached);
+    }
+</script>
+
+<Drawer {heading} bind:shown bind:show onShow={updateLowerBar} bind:hide onHide={revert}>
+    <span style="align-self: flex-start;">{text}</span>
+</Drawer>
---
+++ src/components/__alertDrawerLowerBarComponents/main.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Button from "$components/Button.svelte";
+
+    export let text: string;
+    export let hide: () => void;
+</script>
+
+<Button kind="danger" on:click={hide}>{text}</Button>
